42c95654f2033c9e89b04b87136d630843bba73f
shivaborusu <shivaborusu@gmail.com>
2020-04-05 01:13:42 +0530

    Update california_housing.py

--- California_Housing _Project_file/california_housing.py
+++ California_Housing _Project_file/california_housing.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+"""
+Created on Thu Mar 12 22:41:59 2020
+@author: shivaborusu
+"""
 
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
